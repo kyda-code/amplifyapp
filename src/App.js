8e0db6239065ce3e8fbe6cdd17ad32057b35342b
kyda-code <73664433+kyda-code@users.noreply.github.com>
2021-01-20 10:21:24 -0600

    Branch Dev
    Testing AWS Amplify

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo from './logo.svg'
+import './App.css';
+import {withAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>We now have Auth!</h1>
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <AmplifySignOut/>
+    </div>
+  ); 
+}
+
+export default withAuthenticator(App);
